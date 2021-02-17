74aff49d2f926bb380dc779da62503be39cad9a4
unitworkgray <67017077+unitworkgray@users.noreply.github.com>
2021-02-17 19:08:52 +0300

    Add files via upload
    Добавил разметку во вкладку диалогов

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -4,7 +4,22 @@ import s from './Dialog.module.css';
 const Dialog = ()=> {
     return(
         <div className={s.content}>
-            Диалоги
+            <div className={s.container}>
+                <div className={s.dialogs}>
+                    <div className={s.dialog + ' ' + s.active}>Степа</div>
+                    <div className={s.dialog}>Валера</div>
+                    <div className={s.dialog}>Валентин</div>
+                    <div className={s.dialog}>Гена</div>
+                    <div className={s.dialog}>Дима</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.messege}>Привет</div>
+                    <div className={s.messege}>Привет</div>
+                    <div className={s.messege}>Привет</div>
+                    <div className={s.messege}>Привет</div>
+                    <div className={s.messege}>Привет</div>
+                </div>
+            </div>
         </div>
     );
 }
